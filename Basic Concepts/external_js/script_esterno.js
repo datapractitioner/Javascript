4785e2183344b3028cc657ed1fba6a67ecfe68b9
datapractitioner <teso.tiziano@outlook.it>
2021-03-07 21:09:56 +0100

    array methods
    Other methods on arrays

--- Basic Concepts/external_js/script_esterno.js
+++ Basic Concepts/external_js/script_esterno.js
@@ -5,3 +5,9 @@ function changeColor() {
 function AlertBox() {
     alert("Questa e' una alert box !");
 }
+
+function ArToStr() {
+    // convert array to string
+    var fruits = ['Orange','Lemon','Carrot'];
+    document.getElementById('joined-array').innerHTML = fruits.join(" * ");
+}
